da08fcacb9d4dcbe4fe57edcfe4d5415f670227e
Daniel Ruiz <danielruizgtz@gmail.com>
2021-09-09 20:14:50 -0500

    NW#11 Note Taker App
    In this commit I'm uploading the files and the server with the GET/Notes, GET * and GET/api/notes routs. Missing the POST /api/notes rout.

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var path = require('path')
+const fs = require('fs')
+var app = express()
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+
+app.use(express.static("public"))
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    try {
+        const data = fs.readFileSync(path.join(__dirname,'./db/db.json'), 'utf8')
+        res.json(JSON.parse(data))
+      } catch (err) {
+        console.error(err)
+      }
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
+app.listen(PORT, () => 
+    console.log(`Listening for requests on port ${PORT}!`)
+);
